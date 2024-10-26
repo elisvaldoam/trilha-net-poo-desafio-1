701d8fd276edd8b5452ebb6f303c30f6ee9e22ee
elisvaldoam <78770728+elisvaldoam@users.noreply.github.com>
2024-10-26 10:00:13 -0300

    atividade bootcamp
    Atividade desafio bootcamp

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("2799991111", "Lumia", "123456789", 128);
+n1.InstalarAplicativo("WhatsApp");
+n1.ReceberLigacao();
+
+Iphone i1 = new Iphone("2798989870", "MAX", "23146587", 256);
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Facebook");
